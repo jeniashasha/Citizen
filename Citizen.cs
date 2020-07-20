bd2e52bcdfe217f34c263c09c5d7f2020ab82255
jeniashasha <66595992+jeniashasha@users.noreply.github.com>
2020-07-20 22:38:13 +0300

    Add files via upload

---
+++ Citizen.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Citizen
+    {
+        public string name;
+        public Citizen[] children;
+        public readonly int fatherID;
+        public readonly int id = 0;
+        public static int numberOfCurrentCitizens = 0;
+        public const int MAX_NUM_OF_CITIZENS = 1500000;
+        public Citizen(string name, int fatherID)
+        {
+            this.name = name;
+            numberOfCurrentCitizens++;
+            this.fatherID = fatherID;
+            id = id+1;
+        }
+
+        public static void PrintNumberOfCitizens()
+        {
+            Console.WriteLine($"Number of citizens : {numberOfCurrentCitizens}");
+        }
+        public static bool ReachedHalfOfMaximumSize()
+        {
+            if (numberOfCurrentCitizens >= (MAX_NUM_OF_CITIZENS / 2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+
+        }
+        public void PrintID()
+        {
+            Console.WriteLine($"ID : {this.id}");
+        }
+        public int PrintGapBetweenMyIDAndFather()
+        {
+            return fatherID - id;
+        }
+        public void SetChildren(Citizen[] arr)
+        {
+            this.children = arr;
+            
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()} name : {name}, number of current citizens : {numberOfCurrentCitizens}, father id : {fatherID}, id : {id} ";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        public static bool HasChildren(Citizen citizen)
+        {
+            if (citizen.children.Length == 0)
+            {
+                return false;
+            }
+            return true;
+
+        }
+        public static bool CheckValidity(Citizen citizen)
+        {
+            if (citizen.children.Length != 0)
+            {
+                for (int i = 0; i < citizen.children.Length; i++)
+                {
+                    if (citizen.children[i].fatherID != citizen.id)
+                    {
+                        return false;
+                    }
+                }
+
+            }
+            return true;
+        }
+        
+        static void Main(string[] args)
+        {
+            Citizen c = new Citizen("rafi", 0);
+            Citizen d = new Citizen("john", 1);
+            Citizen h = new Citizen("raz", 1);
+            Citizen p = new Citizen("lilach", 1);
+            Citizen[] children = { d, h, p };
+            c.SetChildren(children);
+            Console.WriteLine(HasChildren(c));
+
+        }
+    }
+}
